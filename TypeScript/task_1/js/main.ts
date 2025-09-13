a729d0e91334e966b7d6b21eab5f1981910b5a94
ilmi-veliu <iveliu011@gmail.com>
2025-09-13 13:15:07 +0200

    T 2

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -50,3 +50,45 @@ function printTeacher(firstName: string, lastName: string): string {
 }
 
 console.log(printTeacher('John', 'Doe'));
+
+ interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student: StudentClassInterface = new StudentClass("John", "Doe");
+console.log(student.displayName());
+console.log(student.workOnHomework());
+
+
